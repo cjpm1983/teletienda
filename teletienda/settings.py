17faf2e958a1744d29f29de5b21c4a96e32d2ceb
Carlos Javier Palacios Morales <cjpm1983@gmail.com>
2023-11-07 16:48:45 -0500

    statics collected on static 3

--- teletienda/settings.py
+++ teletienda/settings.py
@@ -124,9 +124,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "staticfiles",
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
