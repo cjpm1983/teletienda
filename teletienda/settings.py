a16199975cb9e00adce897b64a22fb9e5cd50617
Carlos Javier Palacios Morales <cjpm1983@gmail.com>
2023-11-07 17:39:25 -0500

    agregando en url static como media

--- teletienda/settings.py
+++ teletienda/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7@!37sf0mtiq)hw%b@8@%71m6ky7m0s(8^ymb+)_&@&f80_af*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -119,8 +119,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = "/var/www/teletienda/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = "/var/www/teletienda/static/"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
