5b9ae414aba02e7be94a9b794595e4e870baf832
Carlos J. Palacios <cjpm1983@gmail.com>
2023-11-30 21:36:41 -0500

    debug true

--- teletienda/settings.py
+++ teletienda/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7@!37sf0mtiq)hw%b@8@%71m6ky7m0s(8^ymb+)_&@&f80_af*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
